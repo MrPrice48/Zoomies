f5e94c0da4fb38352d675c48413e30d63aa8f210
Michael Price <mrprice04@gmail.com>
2022-10-25 21:39:28 +1100

    Init v1
    Getting repo working

---
+++ Source/Zoomies/ZoomiesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZoomiesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZOOMIES_API AZoomiesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
